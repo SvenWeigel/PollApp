9a0ca14ea90ac628c39703b06f40207539a2c8bd
Sven <sven.wgl@gmail.com>
2026-04-09 13:30:34 +0200

    add: header and description section

---
+++ src/app/shared/components/buttons/buttons.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons',
+  imports: [],
+  templateUrl: './buttons.html',
+  styleUrl: './buttons.scss',
+})
+export class Buttons {}
---
+++ src/app/shared/components/description/description.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Buttons } from '../buttons/buttons';
+
+@Component({
+  selector: 'app-description',
+  imports: [Buttons],
+  templateUrl: './description.html',
+  styleUrl: './description.scss',
+})
+export class Description {}
