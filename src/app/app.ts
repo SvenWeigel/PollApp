7e7b48047f97373922ed99676d75d6fd7d048ab5
Sven <sven.wgl@gmail.com>
2026-04-09 19:38:08 +0200

    add: components and scss

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/components/header/header";
 import { Description } from "./shared/components/description/description";
+import { YourSurveys } from "./shared/components/your-surveys/your-surveys";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Description],
+  imports: [RouterOutlet, Header, Description, YourSurveys],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/shared/components/your-surveys/your-surveys.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-surveys',
+  imports: [],
+  templateUrl: './your-surveys.html',
+  styleUrl: './your-surveys.scss',
+})
+export class YourSurveys {}
