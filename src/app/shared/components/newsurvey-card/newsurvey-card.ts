54d69101dabcf4771f70905dc2c48775a7d5604a
Sven <sven.wgl@gmail.com>
2026-04-20 21:37:11 +0200

    add: style to card

--- src/app/shared/components/newsurvey-card/newsurvey-card.ts
+++ src/app/shared/components/newsurvey-card/newsurvey-card.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NewsurveyCardHeader } from "../newsurvey-card-header/newsurvey-card-header";
+import { NewsurveyCardForm } from "../newsurvey-card-form/newsurvey-card-form";
 
 @Component({
   selector: 'app-newsurvey-card',
-  imports: [NewsurveyCardHeader,],
+  imports: [NewsurveyCardHeader, NewsurveyCardForm],
   templateUrl: './newsurvey-card.html',
   styleUrl: './newsurvey-card.scss',
 })
