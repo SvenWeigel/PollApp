3ceae1c934c561cfc3fae30fa47e1cb3d61db609
Sven <sven.wgl@gmail.com>
2026-04-20 21:04:20 +0200

    add: router link

--- src/app/shared/components/newsurvey-card-header/newsurvey-card-header.ts
+++ src/app/shared/components/newsurvey-card-header/newsurvey-card-header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Buttons } from "../buttons/buttons";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-newsurvey-card-header',
-  imports: [Buttons],
+  imports: [Buttons, RouterLink],
   templateUrl: './newsurvey-card-header.html',
   styleUrl: './newsurvey-card-header.scss',
 })
