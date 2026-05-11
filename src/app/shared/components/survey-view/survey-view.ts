15441de2eb303dc9fa146e47b147b18f6c856332
Sven <sven.wgl@gmail.com>
2026-05-11 18:22:45 +0200

    add: live view section

--- src/app/shared/components/survey-view/survey-view.ts
+++ src/app/shared/components/survey-view/survey-view.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Buttons } from "../buttons/buttons";
 import { SurveyViewQuestion } from "../survey-view-question/survey-view-question";
+import { SurveyViewChart } from "../survey-view-chart/survey-view-chart";
 
 @Component({
   selector: 'app-survey-view',
-  imports: [Buttons, SurveyViewQuestion],
+  imports: [Buttons, SurveyViewQuestion, SurveyViewChart],
   templateUrl: './survey-view.html',
   styleUrl: './survey-view.scss',
 })
