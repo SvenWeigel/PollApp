d079c06927a837726b285615549d9348f3bba27a
Sven <sven.wgl@gmail.com>
2026-06-03 20:19:25 +0200

    add: filter function to survey list

--- src/app/shared/components/survey-list/survey-list.ts
+++ src/app/shared/components/survey-list/survey-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { SurveyItem } from "../survey-item/survey-item";
 import { Supabase } from '../../../supabase';
 
@@ -10,8 +10,24 @@ import { Supabase } from '../../../supabase';
 })
 export class SurveyList {
   readonly dbService = inject(Supabase);
+  readonly selectedCategory = signal('All');
+  readonly filteredSurveys = computed(() => {
+    const category = this.selectedCategory().toLowerCase();
+    const surveys = this.dbService.surveys();
+
+    if (category === 'all') {
+      return surveys;
+    }
+
+    return surveys.filter((survey) => survey.category.toLowerCase() === category);
+  });
 
   ngOnInit(): void {
     this.dbService.getSurveys();
+  }
+
+  onCategoryChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.selectedCategory.set(target.value);
   }
 }
