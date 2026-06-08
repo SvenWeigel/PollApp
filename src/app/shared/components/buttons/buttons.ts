695ff870876ab503ec1a61027a13a7c1dd604e95
Sven <sven.wgl@gmail.com>
2026-06-08 17:03:02 +0200

    edit: new color and hover effects

--- src/app/shared/components/buttons/buttons.ts
+++ src/app/shared/components/buttons/buttons.ts
@@ -10,4 +10,7 @@ export class Buttons {
   readonly buttonText = input<string>('New survey');
   readonly buttonBgColor = input<string>('var(--color--s-o)');
   readonly showButton = input<boolean>(true);
+  readonly enableHoverEffect = input<boolean>(false);
+  readonly hoverIconSrc = input<string>('');
+  readonly hoverIconAlt = input<string>('');
 }
